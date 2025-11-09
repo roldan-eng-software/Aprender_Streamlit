0e124aaa90ff339398dd4c1b7350738c1838731c
roldan-eng-software <roldan.eng.software@gmail.com>
2025-11-09 19:32:08 -0300

    Feat: mostrar links/botões das páginas em pages/ na sidebar

---
+++ pages/1_📈_Plotting_Demo.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
+st.write(
+    """Esta demonstração ilustra uma combinação de plotagem e animação com o Streamlit. Estamos gerando vários números aleatórios em um loop por cerca de 5 segundos. Aproveite!
+"""
+)
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
